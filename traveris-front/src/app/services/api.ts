28f5ef59333b0d3fb90367eed617b5e3fc7ae03f
pedroagueroo <aguerop47@gmail.com>
2026-02-19 20:32:10 -0300

    Desactivar prerender y ajustar budgets para deploy

--- traveris-front/src/app/services/api.ts
+++ traveris-front/src/app/services/api.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../env';
+import { environment } from '../../environments/env';
 
 @Injectable({
   providedIn: 'root'

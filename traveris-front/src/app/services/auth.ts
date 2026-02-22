45518bc956ad478cb47ec33ac3add3088116cf96
pedroagueroo <aguerop47@gmail.com>
2026-02-22 13:51:54 -0300

    Corregir ruta de auth y vincular a la nube

--- traveris-front/src/app/services/auth.ts
+++ traveris-front/src/app/services/auth.ts
@@ -12,18 +12,17 @@ export class AuthService {
   // Asegúrate de que el servidor Node.js esté corriendo en el puerto 3000
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) { }
 
+  // En tu auth.ts, cambiá la función login para que incluya '/auth'
   login(credenciales: { user: string, pass: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, credenciales).pipe(
+    // Agregamos /auth antes de /login para que coincida con tu backend
+    return this.http.post(`${this.apiUrl}/auth/login`, credenciales).pipe(
       tap((res: any) => {
-        // 1. Guardamos todos los datos de identidad
         localStorage.setItem('token', res.token);
         localStorage.setItem('empresa_nombre', res.empresa_nombre);
         localStorage.setItem('user_rol', res.rol);
         localStorage.setItem('nombre_usuario', res.nombre_usuario);
-
-        // 2. Redirigimos al Dashboard apenas entramos con éxito
         this.router.navigate(['/dashboard']);
       })
     );
@@ -40,11 +39,11 @@ export class AuthService {
   }
 
   getNombreEmpresa(): string {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    return localStorage.getItem('empresa_nombre') || 'Agencia';
+    if (typeof window !== 'undefined' && window.localStorage) {
+      return localStorage.getItem('empresa_nombre') || 'Agencia';
+    }
+    return 'Agencia';
   }
-  return 'Agencia';
-}
 
   // MÉTODO DE CIERRE TOTAL (Soluciona el error del botón "Atrás")
   logout() {
@@ -54,8 +53,8 @@ export class AuthService {
 
     // 2. Navegamos al login borrando el historial de navegación actual
     this.router.navigate(['/login'], { replaceUrl: true }).then(() => {
-        // 3. Recargamos para resetear los estados de los componentes y el Guard
-        window.location.reload();
+      // 3. Recargamos para resetear los estados de los componentes y el Guard
+      window.location.reload();
     });
   }
 }

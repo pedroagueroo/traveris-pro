cb4e22d56c08d2b31b9c41640ae95f299186f690
pedroagueroo <aguerop47@gmail.com>
2026-03-26 00:17:18 -0300

    fix internal db

--- BackTraqveris/src/db.js
+++ BackTraqveris/src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgresql://traveris_user:jCKmw4oehBuzDlJGHeKik7Ne7s22fC5p@dpg-d6bilcur433s73d6dfp0.oregon-postgres.render.com:5432/traveris",
+  connectionString: "postgresql://traveris_user:jCKmw4oehBuzDlJGHeKik7Ne7s22fC5p@dpg-d6bilcur433s73d6dfp0-a/traveris",
   ssl: { rejectUnauthorized: false }
 });
 

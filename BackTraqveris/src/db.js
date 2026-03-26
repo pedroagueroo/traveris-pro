cca10aa2c774856dc56f8f0f92c5ea5fbfeed66b
pedroagueroo <aguerop47@gmail.com>
2026-03-26 00:09:17 -0300

    aaaa

--- BackTraqveris/src/db.js
+++ BackTraqveris/src/db.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config(); // Esto lee el archivo .env
 
 const pool = new Pool({
-  connectionString: "postgresql://traveris_user:jCKmw4oehBuzDlJGHeKik7Ne7s22fC5p@dpg-d6bilcur433s73d6dfp0-a.oregon-postgres.render.com:5432/traveris",
+  connectionString: "postgresql://traveris_user:jCKmw4oehBuzDlJGHeKik7Ne7s22fC5p@dpg-d6bilcur433s73d6dfp0.oregon-postgres.render.com:5432/traveris",
   ssl: { rejectUnauthorized: false }
 });
 

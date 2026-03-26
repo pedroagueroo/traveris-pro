a3d6129ea35d0955ff4c65e303db444efc0f051b
pedroagueroo <aguerop47@gmail.com>
2026-03-26 15:48:18 -0300

    cambios de errores

--- BackTraqveris/src/db.js
+++ BackTraqveris/src/db.js
@@ -1,8 +1,23 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
+// db.js - Versión de diagnóstico
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  user: process.env.DB_USER,
+  host: '127.0.0.1', 
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432, // <--- SI EN EL PASO 1 VISTE 5433, CAMBIALO ACÁ
+  ssl: false 
+});
+
+// Prueba de conexión inmediata al arrancar
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('❌ ERROR CRÍTICO DE CONEXIÓN A LA DB:', err.stack);
+  }
+  console.log('✅ CONEXIÓN EXITOSA A POSTGRESQL LOCAL (agencia_db)');
+  release();
 });
 
 module.exports = pool;
--- traveris-front/src/app/services/auth.ts
+++ traveris-front/src/app/services/auth.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/env';
 
 @Injectable({
   providedIn: 'root'

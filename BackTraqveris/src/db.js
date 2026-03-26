c1e454fd2697155aba646f8ca48344957cd4fe7a
pedroagueroo <aguerop47@gmail.com>
2026-03-26 00:06:06 -0300

    pruebaaa

--- BackTraqveris/src/db.js
+++ BackTraqveris/src/db.js
@@ -2,9 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config(); // Esto lee el archivo .env
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // Esto es vital para Render pero no para Local:
-  ssl: process.env.DATABASE_URL.includes('render.com') ? { rejectUnauthorized: false } : false
+  connectionString: "postgresql://traveris_user:jCKmw4oehBuzDlJGHeKik7Ne7s22fC5p@dpg-d6bilcur433s73d6dfp0-a.oregon-postgres.render.com:5432/traveris",
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
